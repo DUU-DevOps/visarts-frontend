928a4ddc740bd61243d4c8dd76ad3dc35a5ca127
cheriiih <145235547+cheriiih@users.noreply.github.com>
2026-03-14 00:02:50 -0400

    Centered meetUs.tsx

--- src/components/meet-us/meetUs.tsx
+++ src/components/meet-us/meetUs.tsx
@@ -50,9 +50,9 @@ const MeetUs = ({ people }: {
 
     return (
         <Center>
-            <div style={{ width: '100%' }}>
-                <h2 style={{ marginLeft: '20px' }}>VisArts Chair</h2>
-                <Grid gutter="none" mr="0" pr="0" w="100%">
+            <div style={{ width: '100%', maxWidth: '1200px', margin: '0 auto' }}>
+                <h2 style={{ textAlign: 'center', marginBottom: '1rem' }}>VisArts Chair</h2>
+                <Grid gutter="none" justify="center" w="100%">
                     {chairPeople.map((person: any, key: any) => (
                         <GridCol span={{ base: 6, sm: 6, md: 3, lg: 2, xl: 2 }} p={20} key={key} style={{ display: 'flex', justifyContent: 'center' }}>
                             <PersonCard person={person} onClick={() => handleOpen(person)} />
@@ -60,8 +60,8 @@ const MeetUs = ({ people }: {
                     ))}
                 </Grid>
 
-                <h2 style={{ marginLeft: '20px' }}>Executive Board</h2>
-                <Grid gutter="none" mr="0" pr="0" w="100%">
+                <h2 style={{ textAlign: 'center', marginBottom: '1rem', marginTop: '2rem' }}>Executive Board</h2>
+                <Grid gutter="none" justify="center" w="100%">
                     {execPeople.map((person: any, key: any) => (
                         <GridCol span={{ base: 6, sm: 6, md: 3, lg: 2, xl: 2 }} p={20} key={key} style={{ display: 'flex', justifyContent: 'center' }}>
                             <PersonCard person={person} onClick={() => handleOpen(person)} />
@@ -69,8 +69,8 @@ const MeetUs = ({ people }: {
                     ))}
                 </Grid>
 
-                <h2 style={{ marginLeft: '20px' }}>Committee Members</h2>
-                <Grid gutter="none" mr="0" pr="0" w="100%">
+                <h2 style={{ textAlign: 'center', marginBottom: '1rem', marginTop: '2rem' }}>Committee Members</h2>
+                <Grid gutter="none" justify="center" w="100%">
                     {committeePeople.map((person: any, key: any) => (
                         <GridCol span={{ base: 6, sm: 6, md: 3, lg: 2, xl: 2 }} p={20} key={key} style={{ display: 'flex', justifyContent: 'center' }}>
                             <PersonCard person={person} onClick={() => handleOpen(person)} />
