80a52b6da5f7e02c0f45c158bb83bb679b2458a1
cheriiih <145235547+cheriiih@users.noreply.github.com>
2026-03-14 00:15:05 -0400

    Structure fix meetUs.tsx

--- src/components/meet-us/meetUs.tsx
+++ src/components/meet-us/meetUs.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react'
-import { Grid, GridCol, Center } from "@mantine/core";
+import { Grid, GridCol } from "@mantine/core";
 import PersonCard from '@/components/meet-us/personCard';
 import { useState } from 'react';
 import PersonModal from './personModal';
@@ -23,11 +23,14 @@ const MeetUs = ({ people }: {
         order: 0,
         section: ''
     }
+
     const [opened, setOpened] = useState(false);
     const [selected, setSelected] = useState(emptyPerson);
+
     const handleClose = () => {
         setOpened(false);
     }
+
     const handleOpen = (person: {
         name: string,
         bio: string
@@ -49,40 +52,38 @@ const MeetUs = ({ people }: {
     const committeePeople = people.filter((person) => person.section === 'member');
 
     return (
-        <Center>
-            <div style={{ width: '100%', maxWidth: '1200px', margin: '0 auto' }}>
-                <h2 style={{ textAlign: 'center', marginBottom: '1rem' }}>VisArts Chair</h2>
-                <Grid gutter="none" justify="center" w="100%">
-                    {chairPeople.map((person: any, key: any) => (
-                        <GridCol span={{ base: 6, sm: 6, md: 3, lg: 2, xl: 2 }} p={20} key={key} style={{ display: 'flex', justifyContent: 'center' }}>
-                            <PersonCard person={person} onClick={() => handleOpen(person)} />
-                        </GridCol>
-                    ))}
-                </Grid>
+        <div style={{ width: '100%' }}>
+            <h2 style={{ textAlign: 'center' }}>VisArts Chair</h2>
+            <Grid justify="center">
+                {chairPeople.map((person: any, key: any) => (
+                    <GridCol span={{ base: 12, sm: 6, md: 4, lg: 3, xl: 3 }} p={20} key={key}>
+                        <PersonCard person={person} onClick={() => handleOpen(person)} />
+                    </GridCol>
+                ))}
+            </Grid>
 
-                <h2 style={{ textAlign: 'center', marginBottom: '1rem', marginTop: '2rem' }}>Executive Board</h2>
-                <Grid gutter="none" justify="center" w="100%">
-                    {execPeople.map((person: any, key: any) => (
-                        <GridCol span={{ base: 6, sm: 6, md: 3, lg: 2, xl: 2 }} p={20} key={key} style={{ display: 'flex', justifyContent: 'center' }}>
-                            <PersonCard person={person} onClick={() => handleOpen(person)} />
-                        </GridCol>
-                    ))}
-                </Grid>
+            <h2 style={{ textAlign: 'center', marginTop: '2rem' }}>Executive Board</h2>
+            <Grid justify="center">
+                {execPeople.map((person: any, key: any) => (
+                    <GridCol span={{ base: 12, sm: 6, md: 4, lg: 3, xl: 3 }} p={20} key={key}>
+                        <PersonCard person={person} onClick={() => handleOpen(person)} />
+                    </GridCol>
+                ))}
+            </Grid>
 
-                <h2 style={{ textAlign: 'center', marginBottom: '1rem', marginTop: '2rem' }}>Committee Members</h2>
-                <Grid gutter="none" justify="center" w="100%">
-                    {committeePeople.map((person: any, key: any) => (
-                        <GridCol span={{ base: 6, sm: 6, md: 3, lg: 2, xl: 2 }} p={20} key={key} style={{ display: 'flex', justifyContent: 'center' }}>
-                            <PersonCard person={person} onClick={() => handleOpen(person)} />
-                        </GridCol>
-                    ))}
-                </Grid>
-            </div>
+            <h2 style={{ textAlign: 'center', marginTop: '2rem' }}>Committee Members</h2>
+            <Grid justify="center">
+                {committeePeople.map((person: any, key: any) => (
+                    <GridCol span={{ base: 12, sm: 6, md: 4, lg: 3, xl: 3 }} p={20} key={key}>
+                        <PersonCard person={person} onClick={() => handleOpen(person)} />
+                    </GridCol>
+                ))}
+            </Grid>
 
             {selected &&
                 <PersonModal person={selected} opened={opened} close={handleClose} title="About" />
             }
-        </Center>
+        </div>
     )
 }
 
